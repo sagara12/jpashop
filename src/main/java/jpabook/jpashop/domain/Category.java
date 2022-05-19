7ce425e8a4e421b2c537b494b9d5c190c0a6726a
sagara12 <sagara12@naver.com>
2022-05-19 18:02:27 +0900

    2022-05-15 프로젝트 셋팅 수정 2022-05-16 타임리프 템플릿 세팅 및 devtools 모듈 설치 2022-05-17 DB 설정 및 JPA 설정 p6psy 라이브러리 설치 2022-05-18 DB 엔티티 클래스 생성 2022-05-18 지연처리 설정 및 연관 관계 처리 메서드 작성

--- src/main/java/jpabook/jpashop/domain/Category.java
+++ src/main/java/jpabook/jpashop/domain/Category.java
@@ -18,14 +18,14 @@ public class Category {
 
     private String name;
 
-    @ManyToMany
+    @ManyToMany(fetch = FetchType.LAZY)
     @JoinTable(name = "category_item",
         joinColumns = @JoinColumn(name = "category_id"),
             inverseJoinColumns = @JoinColumn(name = "item_id")
     )
     private List<Item> items =  new ArrayList<>();
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "parent_id")
     private Category parent;
 
--- src/main/java/jpabook/jpashop/domain/Member.java
+++ src/main/java/jpabook/jpashop/domain/Member.java
@@ -20,7 +20,9 @@ public class Member {
     @Embedded
     private Address address;
 
-    @OneToMany(mappedBy = "member")
+    @OneToMany(mappedBy = "member", fetch = FetchType.LAZY)
     private List<Order> orders = new ArrayList<>();
+
+
 
 }
