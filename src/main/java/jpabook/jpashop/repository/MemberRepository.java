fcd0dd3a072d9dc64a22cc5d6a1769a9aedbcf70
sagara12 <sagara12@naver.com>
2022-05-29 18:54:01 +0900

    2022-05-15 프로젝트 셋팅 수정 2022-05-16 타임리프 템플릿 세팅 및 devtools 모듈 설치 2022-05-17 DB 설정 및 JPA 설정 p6psy 라이브러리 설치 2022-05-18 DB 엔티티 클래스 생성 2022-05-18 지연처리 설정 및 연관 관계 처리 메서드 작성 2022-05-21 회원 리포지토리 개발 2022-05-29 회원 서비스 개발

--- src/main/java/jpabook/jpashop/repository/MemberRepository.java
+++ src/main/java/jpabook/jpashop/repository/MemberRepository.java
@@ -1,6 +1,7 @@
 package jpabook.jpashop.repository;
 
 import jpabook.jpashop.domain.Member;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
@@ -9,10 +10,11 @@ import javax.xml.transform.Result;
 import java.util.List;
 
 @Repository
+@RequiredArgsConstructor
 public class MemberRepository {
 
-    @PersistenceContext
-    private EntityManager em;
+//  @PersistenceContext
+    private final EntityManager em;
 
     public void  save(Member member){
         em.persist(member);
@@ -22,7 +24,7 @@ public class MemberRepository {
         return em.find(Member.class, id);
     }
 
-    public List<Member> findall(){
+    public List<Member> findAll(){
         return em.createNamedQuery("select m from Member m", Member.class)
                 .getResultList();
     }
