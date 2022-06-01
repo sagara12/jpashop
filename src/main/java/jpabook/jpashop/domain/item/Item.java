a479bd5492087a29c5a53428e6b8f122a9a872f4
sagara12 <sagara12@naver.com>
2022-06-01 19:12:45 +0900

    2022-05-15 프로젝트 셋팅 수정 2022-05-16 타임리프 템플릿 세팅 및 devtools 모듈 설치 2022-05-17 DB 설정 및 JPA 설정 p6psy 라이브러리 설치 2022-05-18 DB 엔티티 클래스 생성 2022-05-18 지연처리 설정 및 연관 관계 처리 메서드 작성 2022-05-21 회원 리포지토리 개발 2022-05-29 회원 서비스 개발 2022-05-29 회원 테스트 개발 및 테스트 환경 격리 2022-06-01 상품 레포지토리 개발

--- src/main/java/jpabook/jpashop/domain/item/Item.java
+++ src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -1,6 +1,7 @@
 package jpabook.jpashop.domain.item;
 
 import jpabook.jpashop.domain.Category;
+import jpabook.jpashop.exception.NotEnoughStockException;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -26,6 +27,18 @@ public abstract class Item {
     @ManyToMany(mappedBy = "items")
     private List<Category> categories = new ArrayList<Category>();
 
+    //비지니스로직
 
+    //stock 증가
+    public void addStock(int quantity) {
+        this.stockQuantity += quantity;
+    }
 
+    public void removeStock(int quantity){
+        int restStock = this.stockQuantity - quantity;
+        if (restStock < 0){
+            throw new NotEnoughStockException("need more stock");
+        }
+        this.stockQuantity = restStock;
+    }
 }
