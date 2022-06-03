906c84b27e9e34fb77edcc08b81dc890ba05ff16
sagara12 <sagara12@naver.com>
2022-06-03 18:42:25 +0900

    2022-05-15 프로젝트 셋팅 수정 2022-05-16 타임리프 템플릿 세팅 및 devtools 모듈 설치 2022-05-17 DB 설정 및 JPA 설정 p6psy 라이브러리 설치 2022-05-18 DB 엔티티 클래스 생성 2022-05-18 지연처리 설정 및 연관 관계 처리 메서드 작성 2022-05-21 회원 리포지토리 개발 2022-05-29 회원 서비스 개발 2022-05-29 회원 테스트 개발 및 테스트 환경 격리 2022-06-01 상품 엔티티 개발 2022-06-01 상품 레포지토리 개발 2022-06-02 상품 서비스개발 2022-06-03 주문 상품 엔티티 개발 2022-06-03 주문 리포지토리 개발 2022-06-03 주문 서비스 개발

--- src/main/java/jpabook/jpashop/domain/OrderItem.java
+++ src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -1,7 +1,9 @@
 package jpabook.jpashop.domain;
 
 import jpabook.jpashop.domain.item.Item;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
@@ -9,6 +11,7 @@ import javax.persistence.*;
 @Entity
 @Table(name = "order_item")
 @Getter @Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class OrderItem {
 
     @Id @GeneratedValue
@@ -25,6 +28,7 @@ public class OrderItem {
 
     private int orderPrice;
     private int count;
+
     //==생성 메서드
     public static OrderItem createOrderItem(Item item, int orderPrice, int count) {
         OrderItem orderItem = new OrderItem();
