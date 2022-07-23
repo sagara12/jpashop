45949e60f8b9c40593125fa48b27767731a35d78
sagara12 <sagara12@naver.com>
2022-07-23 21:45:38 +0900

    2022-05-15 프로젝트 셋팅 수정 2022-05-16 타임리프 템플릿 세팅 및 devtools 모듈 설치 2022-05-17 DB 설정 및 JPA 설정 p6psy 라이브러리 설치 2022-05-18 DB 엔티티 클래스 생성 2022-05-18 지연처리 설정 및 연관 관계 처리 메서드 작성 2022-05-21 회원 리포지토리 개발 2022-05-29 회원 서비스 개발 2022-05-29 회원 테스트 개발 및 테스트 환경 격리 2022-06-01 상품 엔티티 개발 2022-06-01 상품 레포지토리 개발 2022-06-02 상품 서비스개발 2022-06-03 주문 상품 엔티티 개발 2022-06-03 주문 리포지토리 개발 2022-06-03 주문 서비스 개발 2022-06-03 주문 기능 테스트 2022-06-04 주문 검색 기능 개발 (동적 쿼리) 2022-06-04 홈 화면과 레이아웃 개발 2022-06-04 회원 목록 개발 2022-06-05 회원 목록 조회 2022-06-06 상품 등록 및 수정 2022-06-06 상품 수정 버튼 기능 2022-06-06 상품 수정 버튼 기능구현 2022-06-06 변경 감지와 병합 2022-06-06 주문목록검색, 취소 2022-07-23 회원 등록 API 만들기 2022-07-23 회원 수정 API 만들기

--- src/main/java/jpabook/jpashop/api/MemberApiController.java
+++ src/main/java/jpabook/jpashop/api/MemberApiController.java
@@ -2,12 +2,11 @@ package jpabook.jpashop.api;
 
 import jpabook.jpashop.domain.Member;
 import jpabook.jpashop.service.MemberService;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import javax.validation.constraints.NotEmpty;
@@ -40,6 +39,30 @@ public class MemberApiController {
         Long id = memberService.join(member);
         return new CreateMemberResponse(id);
     }
+
+    @PutMapping("/api/v2/members/{id}")
+    public UpdateDateResponse  updateMemberV2(
+            @PathVariable("id") Long id,
+            @RequestBody @Valid UpdateMemberRequest request){
+
+            memberService.update(id, request.getName());
+            Member findMember = memberService.findOne(id);
+            return new UpdateDateResponse(findMember.getId(), findMember.getName());
+
+    }
+
+    @Data
+    static class UpdateMemberRequest{
+        private String name;
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class UpdateDateResponse{
+        private Long id;
+        private String name;
+    }
+
 
     @Data
     static class CreateMemberRequest {
